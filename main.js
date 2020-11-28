4725d22e1ce2bff33b21d0efc4be18c830e2e8ee
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-11-28 17:21:16 +0200

    commit text

---
+++ main.js
@@ -0,0 +1,43 @@
+var tempRange = document.getElementById("range").value;
+var tempNum = document.getElementById("number").value;
+
+var range;
+var num;
+var ost = 0;
+
+setInterval(function() {
+    range = +document.getElementById("range").value;
+    num = +document.getElementById("number").value;
+
+    if (tempRange !== range) {
+        tempRange = range;
+
+        document.getElementById('number').value = range;
+    }
+    else if (tempNum !== num) {
+        tempNum = num;
+
+        document.getElementById('range').value = num;
+    }
+
+    if (range < 20) {
+        ost = 2;
+    }
+    else if (range >= 20 && range < 50) {
+        ost = 4;
+    }
+    else if (range >= 50 && range < 75) {
+        ost = 6;
+    }
+    else if (range >= 75 && range <= 100) {
+        ost = 8;
+    }      
+
+    console.log(num, "num");
+    console.log(range, "range");
+    console.log(ost, "ost");
+    console.log(range-ost, "range-ost")
+
+    document.getElementById("block").innerHTML = `<div class="div" style="background: linear-gradient(to top, 
+        green ${range-ost}%, red ${range-ost}% ${range}%, #FFF ${range}%);"></div>`;
+}, 500);
